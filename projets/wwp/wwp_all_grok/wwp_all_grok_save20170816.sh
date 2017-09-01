68913eeee73b1e219900946d7620fadd6280ee7b
Louis Munier <louis.munier@epfl.ch>
2017-09-01 14:40:22 +0200

    gestion des anciens fichiers

--- projets/wwp/wwp_all_grok/wwp_all_grok_save20170816.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#petit script à lancer pour que cela tourne !
-#zf170808.1735
-
-export LS_HEAP_SIZE="80m"
-curl -XDELETE http://10.92.104.198:9200/*
-/opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_all.conf
