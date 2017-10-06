6d0c7b8694dae1d151e975f9b6aa5a99b9050b1b
Christian Zufferey <christian@zufferey.com>
2017-10-06 15:17:46 +0200

    - voilà le script de récupération des logs depuis le nassdf1 marche bien, il fait environ 3.5mn pour tout importer

--- projets/www_epfl/install_logs.sh
+++ projets/www_epfl/install_logs.sh
@@ -1,51 +1,77 @@
 #!/bin/bash
 #petit script pour installer très facilement les logs qui se trouve sur le NAS de la SDF
-#zf171006.1034
+#zf171006.1512
 
-#sudo apt-get update
-#sudo apt-get -y install sshpass
-#sudo apt-get -y install -y gnupg2
+zPATHprojet=~/logs_www_epfl
+zPATHnas=/volume1/sdf/logs_www_epfl/170919
 
-#./acb_uncrypt.sh
+sudo apt-get update
+sudo apt-get -y install sshpass tree
+sudo apt-get -y install -y gnupg2
 
+./acb_uncrypt.sh
 . ~/app_crendential_box.elk_log.sh
-#echo $zPASSWORD
+echo $zPASSWORD
 
-zPATH=~/logs_www_epfl
-
-
-
-#mkdir $zPATH
-echo --------------- copie les logs depuis le nassdf1, cela prend environ 1 minute !
+rm -R $zPATHprojet
+mkdir $zPATHprojet
 
 
-y'a encore un gros problème ici entre le path de la vm et celui du NAS zf171006.1043
-exit
+zPATH=static1_logs/var/log/nginx
+zFILE=access.log-20170920
+echo -e --------------- copie $zPATH/$zFILE depuis le nassdf1, cela prend environ 1 minute !
+mkdir -p $zPATHprojet/$zPATH
+sshpass -p $zPASSWORD scp sdf@128.178.116.159:/$zPATHnas/$zPATH/$zFILE $zPATHprojet/$zPATH/.
+echo -
+echo -e --------------- nombre de lignes de $zPATHprojet/$zPATH/$zFILE
+wc $zPATHprojet/$zPATH/$zFILE
+echo -e --------------- première ligne de $zPATHprojet/$zPATH/$zFILE
+head -n 1 $zPATHprojet/$zPATH/$zFILE
+echo -e --------------- dernière ligne de $zPATHprojet/$zPATH/$zFILE
+tail -n 1 $zPATHprojet/$zPATH/$zFILE
+echo -
+head -n 10 $zPATHprojet/$zPATH/$zFILE > $zPATHprojet/$zPATH/$zFILE.10l
+
+
+zPATH=static2_logs/var/log/nginx
+zFILE=access.log-20170920
+echo -e --------------- copie $zPATH/$zFILE depuis le nassdf1, cela prend environ 1 minute !
+mkdir -p $zPATHprojet/$zPATH
+sshpass -p $zPASSWORD scp sdf@128.178.116.159:/$zPATHnas/$zPATH/$zFILE $zPATHprojet/$zPATH/.
+echo -
+echo -e --------------- nombre de lignes de $zPATHprojet/$zPATH/$zFILE
+wc $zPATHprojet/$zPATH/$zFILE
+echo -e --------------- première ligne de $zPATHprojet/$zPATH/$zFILE
+head -n 1 $zPATHprojet/$zPATH/$zFILE
+echo -e --------------- dernière ligne de $zPATHprojet/$zPATH/$zFILE
+tail -n 1 $zPATHprojet/$zPATH/$zFILE
+echo -
+head -n 10 $zPATHprojet/$zPATH/$zFILE > $zPATHprojet/$zPATH/$zFILE.10l
+
+
+zPATH=www_logs/www_logs
+zFILE=www.epfl.ch-ssl_access_ssl.log
+echo -e --------------- copie $zPATH/$zFILE depuis le nassdf1, cela prend environ 1 minute !
+mkdir -p $zPATHprojet/$zPATH
+sshpass -p $zPASSWORD scp sdf@128.178.116.159:/$zPATHnas/$zPATH/$zFILE $zPATHprojet/$zPATH/.
+echo -
+echo -e --------------- nombre de lignes de $zPATHprojet/$zPATH/$zFILE
+wc $zPATHprojet/$zPATH/$zFILE
+echo -e --------------- première ligne de $zPATHprojet/$zPATH/$zFILE
+head -n 1 $zPATHprojet/$zPATH/$zFILE
+echo -e --------------- dernière ligne de $zPATHprojet/$zPATH/$zFILE
+tail -n 1 $zPATHprojet/$zPATH/$zFILE
+echo -
+head -n 10 $zPATHprojet/$zPATH/$zFILE > $zPATHprojet/$zPATH/$zFILE.10l
+
+tree $zPATHprojet
 
 
 
-zPATH2=$zPATH/static1_logs/var/log/nginx
-zFILE=$zPATH2/access.log-20170920
-
-sshpass -p $zPASSWORD scp sdf@128.178.116.159:/volume1/sdf/logs_www_epfl/170919/$zFILE $zPATH/.
-
-
-echo -e --------------- head $zFILE
-head -n 1 $zFILE
-echo -e --------------- tail $zFILE
-tail -n 1 $zFILE
 
 
 
-: 'zdebug'
-
-zPATH2=$zPATH/static2_logs/var/log/nginx
-zFILE=$zPATH2/access.log-20170920
-echo -e --------------- head $zFILE
-head -n 1 $zFILE
-echo -e --------------- tail $zFILE
-tail -n 1 $zFILE
-
+: <<'zdebug'
 zPATH2=$zPATH/www_logs/www_logs
 zFILE=$zPATH2/www.epfl.ch-ssl_access_ssl.log
 echo -e --------------- head $zFILE
