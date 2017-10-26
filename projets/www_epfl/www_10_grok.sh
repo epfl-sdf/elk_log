b5618b39d2923d1ad9e878d0500f81e16fc5350b
Christian Zufferey <christian@zufferey.com>
2017-10-26 19:23:46 +0200

    - modifié mon script pour qu'il fonctionne ;-)

--- projets/www_epfl/www_10_grok.sh
+++ projets/www_epfl/www_10_grok.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #petit script à lancer pour que cela tourne !
-#zf171006.1526
+#zf171026.1725
 
 
 #curl -XDELETE http://sdf-elk-dev:9200/*
-/opt/logstash/bin/logstash -f /home/ubuntu/elk_log/projets/www_epfl/www_10_grok.conf
+/usr/share/logstash/bin/logstash -f /home/ubuntu/elk_log/projets/www_epfl/www_10_grok.conf
 
