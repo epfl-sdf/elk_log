4f9be912f1b515f2dc4b27d7a57c9c739290cd3e
Christian Zufferey <christian@zufferey.com>
2017-10-27 16:22:56 +0200

    - modifié le script projets/www_epfl/www_10_grok.conf pour lancer toute la sauce ;-) - amélioré le script projets/www_epfl/www_10_grok.sh pour la partie lancement dans un 'screen'

--- projets/www_epfl/www_10_grok.sh
+++ projets/www_epfl/www_10_grok.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 #petit script à lancer pour que cela tourne !
-#zf171026.1725
+#zf171027.1619
 
 
-#curl -XDELETE http://sdf-elk-dev:9200/*
+THEIP=$(/sbin/ifconfig ens18 | /bin/grep "inet ad" | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
+echo -e " 
+Afin de pouvoir garder logstash en marche tout en pouvant quitter la console, il serait bien de le faire tourner dans un 'screen' avec:
+screen -S logstash    pour entrer dans screen
+./start.sh            pour lancer le serveur WEB dans screen
+CTRL+a,d              pour sortir de screen en laissant tourner le serveur
+screen -r logstash    pour revenir dans screen
+screen -x logstash    pour revenir à plusieurs dans screen
+CTRL+d                pour terminer screen
+screen -list          pour lister tous les screens en fonctionement
+
+On peur voir les résultat sur Kibana avec:
+http://$THEIP:5601
+"
+read -p "appuyer une touche pour démarrer Logstash"
+
+
+#attention ceci efface toute la DB Elasticsearch
+#curl -XDELETE http://zf-2:9200/*
+#autrement il faut faire pour effacer que l'index
+#curl -XDELETE http://zf-2:9200/lindixquisetrouvedansleconf
+
 /usr/share/logstash/bin/logstash -f /home/ubuntu/elk_log/projets/www_epfl/www_10_grok.conf
 
