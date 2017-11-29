66abb655fbef1b4e57aefc37c86848edea819b76
Christian Zufferey <christian@zufferey.com>
2017-11-29 19:58:04 +0100

    - corrigé le problème du montage des serveurs de kiss_logs

--- projets/www_epfl/kis_logs_mount.sh
+++ projets/www_epfl/kis_logs_mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #petit script pour monter très facilement les logs qui se trouvent sur les serveurs du KIS en sshfs
-#zf171129.1803
+#zf171129.1952
 
 
 #serveur pour http://www-test.epfl.ch
@@ -17,11 +17,12 @@ sudo chown -R ubuntu:ubuntu /mnt/log_remote/exopgesrv76/
 sshfs kis@exopgesrv76:/var/www/vhosts/www.epfl.ch/logs /mnt/log_remote/exopgesrv76/www.epfl.ch
 
 #serveur pour http://static.epfl.ch
-sudo mkdir -p /mnt/log_remote/exopgesrv104
-sudo chown -R ubuntu:ubuntu /mnt/log_remote/exopgesrv104
-sshfs kis@exopgesrv104:/var/log/nginx /mnt/log_remote/exopgesrv104
-sudo mkdir -p /mnt/log_remote/exopgesrv105
-sudo chown -R ubuntu:ubuntu /mnt/log_remote/exopgesrv105
-sshfs kis@exopgesrv105:/var/log/nginx /mnt/log_remote/exopgesrv105
+sudo mkdir -p /mnt/log_remote/kissrv104
+sudo chown -R ubuntu:ubuntu /mnt/log_remote/kissrv104
+sshfs kis@kissrv104:/var/log/nginx /mnt/log_remote/kissrv104
+sudo mkdir -p /mnt/log_remote/kissrv105
+sudo chown -R ubuntu:ubuntu /mnt/log_remote/kissrv105
+sshfs kis@kissrv105:/var/log/nginx /mnt/log_remote/kissrv105
 
+mount
 
